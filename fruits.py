92d88e62a51173f72ff22a101005c6622bd1a01c
David Wong <rmhzdtw@ucl.ac.uk>
2021-05-16 22:04:41 +0100

    added total function

--- fruits.py
+++ fruits.py
@@ -1,5 +1,10 @@
 import re
 
+# function to count words in string contents
+def word_count(string):
+    return(len(string.strip().split(" ")))
+
+
 # open file to write, create the file if necessary
 print ("Reading data set 1")
 f=open("mydata.txt", "w+")
@@ -15,6 +20,9 @@ f=open("mydata.txt", "r")
 if f.mode == 'r':
     contents=f.read()
     print("Fruits:",re.sub(" ", ", ", contents))
+
+# Give total quantity
+print("Total Fruits:",word_count(contents))
 
 # close the file 
 f.close()
